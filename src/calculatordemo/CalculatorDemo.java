bdf28356bf9219a4365af139de55097af148b566
rishabhgupta97 <40624419+rishabhgupta97@users.noreply.github.com>
2018-07-02 09:53:29 -0600

    DEV:001 -Test Commit Commited by Rishabh Reviwed by Govind

---
+++ src/calculatordemo/CalculatorDemo.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculatordemo;
+
+/**
+ *
+ * @author root
+ */
+public class CalculatorDemo {
+
+   public void show(){
+       System.out.println("hello");
+       System.out.println("hello");
+       System.out.println("hello");
+       System.out.println("hello");
+       System.out.println("hello");
+       System.out.println("hello");
+       System.out.println("hello");
+   }
+   public void print(){
+       System.out.println("hello");
+   }
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
